0af067926ca2a301a4526a9cd4a3cd52732d3365
Cailyn <cailyn.s.e@gmail.com>
2017-03-11 17:43:56 -0500

    get key

--- WhereforeArtThou.js
+++ WhereforeArtThou.js
@@ -1,8 +1,12 @@
 function whatIsInAName(collection, source) {
 	// What's in a name?
-	var arr = [];
+	var arr = [],
+		key = Object.keys(source);
+	console.log("Key is: " + key);
 	// Only change code below this line
+	for (var i = 0; i < source.length; i++) {
 
+	}
 
 	// Only change code above this line
 	return arr;
