b2fa6c4f5ca90bd632afb442028fd3665460f9ba
Cailyn <cailyn.s.e@gmail.com>
2017-03-11 18:09:09 -0500

    check which objects have the given property

--- WhereforeArtThou.js
+++ WhereforeArtThou.js
@@ -2,12 +2,28 @@ function whatIsInAName(collection, source) {
 	// What's in a name?
 	var arr = [],
 		key = Object.keys(source);
-	console.log("Key is: " + key);
+	console.log("Key is: ", key);
 	// Only change code below this line
-	for (var i = 0; i < source.length; i++) {
-
+	for (var i = 0; i < collection.length; i++) {
+		console.log("object is", collection[i]);
+		if (key in collection[i]) {
+			console.log(collection[i][key]);
+		}
 	}
 
 	// Only change code above this line
 	return arr;
 }
+
+whatIsInAName([{
+	first: "Romeo",
+	last: "Montague"
+}, {
+	first: "Mercutio",
+	last: null
+}, {
+	first: "Tybalt",
+	last: "Capulet"
+}], {
+	last: "Capulet"
+});
