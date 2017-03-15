268f4b0d2dac57bc1bcbd5e2f11210bda5550ea8
Cailyn <cailyn.s.e@gmail.com>
2017-03-15 16:55:26 -0400

    added Check vowel function
    This function returns true if the character given is a vowel and false if not.

--- pigLatin.js
+++ pigLatin.js
@@ -1,4 +1,13 @@
 function translatePigLatin(str) {
+	function checkVowel(char) {
+		var vowels = ["a", "e", "i", "o", "u"];
+		for (var i = 0; i < vowels.length; i++) {
+			if (char === vowels[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
 	return str;
 }
 
