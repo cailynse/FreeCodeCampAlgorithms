3529c971b85c1400636133411854a4735a015b73
Cailyn <cailyn.s.e@gmail.com>
2017-03-18 21:50:36 -0400

    Finished!

--- DNApairing.js
+++ DNApairing.js
@@ -1,5 +1,19 @@
 function pairElement(str) {
-	return str;
+
+	var pairs = {
+			G: "C",
+			C: "C",
+			A: "T",
+			T: "A"
+		},
+		newStr;
+
+	for (var i = 0; i < str.length; i++) {
+		newStr.push(str[i]);
+		newStr.push(pairs[str[i]]);
+	}
+
+	return newStr;
 
 }
 
