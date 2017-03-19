40c78131e577410ed870dd9ccc6720da92b8f31b
Cailyn <cailyn.s.e@gmail.com>
2017-03-18 22:34:21 -0400

    Finished

--- missingLetters.js
+++ missingLetters.js
@@ -1,5 +1,15 @@
 function fearNotLetter(str) {
-	return str;
+	var charCodes = [],
+		newStr;
+	for (var i = 0; i < str.length; i++) {
+		charCodes.push(str.charCodeAt(i));
+	}
+	for (i = 0; i < charCodes.length - 1; i++) {
+		if (charCodes[i] !== charCodes[i + 1] - 1) {
+			return String.fromCharCode(charCodes[i] + 1);
+		}
+	}
+	return undefined;
 }
 
 fearNotLetter("abce");
