5f9b0bd9dc6af1a860c8dd92e7a1ee8f70cb8bab
Cailyn <cailyn.s.e@gmail.com>
2017-03-18 22:44:57 -0400

    Finished!

--- booWho.js
+++ booWho.js
@@ -1,6 +1,11 @@
 function booWho(bool) {
 	// What is the new fad diet for ghost developers? The Boolean.
-	return bool;
+	'use strict';
+	if (bool === true || bool === false) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 booWho(null);
