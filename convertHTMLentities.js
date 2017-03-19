7f0e88d1c0f5ad921a061fe11dee8fa695353c12
Cailyn <cailyn.s.e@gmail.com>
2017-03-19 00:12:56 -0400

    Finished!

--- convertHTMLentities.js
+++ convertHTMLentities.js
@@ -1,6 +1,16 @@
 function convertHTML(str) {
 	// &colon;&rpar;
-	return str;
+	var HTMLentities = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'\"': '&quot;',
+		"\'": '&apos;'
+	};
+	return str.split('').map(function (symbol) {
+		return HTMLentities[symbol] || symbol;
+	}).join('');
+
 }
 
 convertHTML("Dolce & Gabbana");
