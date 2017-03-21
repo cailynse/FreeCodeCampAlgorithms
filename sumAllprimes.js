26e004a35d4a26eef9dcaa5e7460e72afc7d9d64
Cailyn <cailyn.s.e@gmail.com>
2017-03-21 12:44:43 -0400

    Finished!

--- sumAllprimes.js
+++ sumAllprimes.js
@@ -1,5 +1,28 @@
 function sumPrimes(num) {
-	return num;
+	var numArr = [];
+
+	function isPrime(val) {
+		if (val > 1) {
+			if (val > 2) {
+				for (var i = 2; i < val; i++) {
+					if (val % i === 0) {
+						return false;
+					}
+				}
+				return true;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	for (var i = 0; i <= num; i++) {
+		if (isPrime(i)) {
+			numArr.push(i);
+		}
+	}
+	var sum = numArr.reduce((a, b) => a + b, 0);
+	return sum;
 }
 
 sumPrimes(10);
