6b92358f3f6e004c3507bfdf19c8e0353cb580ea
Cailyn <cailyn.s.e@gmail.com>
2017-03-22 13:07:34 -0400

    sorted list created

--- smallestCommons.js
+++ smallestCommons.js
@@ -1,7 +1,32 @@
-
 function smallestCommons(arr) {
-  return arr;
+	var sortedArr = arr,
+		numsBetween = [];
+
+	function compare(a, b) {
+		return a - b;
+	}
+	sortedArr.sort(compare);
+
+	for (var i = arr[0]; i <= arr[1]; i++) {
+		numsBetween.push(i);
+	}
+
+	console.log(numsBetween);
+
+	// Euclid's Algorithm 
+
+	var gcd = function (a, b) {
+		if (!b) {
+			return a;
+		}
+
+		return gcd(b, a % b);
+	};
+	console.log(gcd(462, 910));
+
+
+	return arr;
 }
 
 
-smallestCommons([1,5]);
+smallestCommons([1, 5]);
