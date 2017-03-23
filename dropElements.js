1da31fe881a6bc5e60c867d6166a174406f94c51
Cailyn <cailyn.s.e@gmail.com>
2017-03-23 11:27:42 -0400

    Finished!

--- dropElements.js
+++ dropElements.js
@@ -1,6 +1,17 @@
 function dropElements(arr, func) {
-  // Drop them elements.
-  return arr;
+    // Drop them elements.
+    var newArr = arr.slice(0);
+
+    for (var i = 0; i < arr.length; i++) {
+        if (!func(newArr[0])) {
+            newArr.shift();
+        } else {
+            return newArr;
+        }
+    }
+    return newArr;
 }
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+dropElements([1, 2, 3], function (n) {
+    return n < 3;
+});
