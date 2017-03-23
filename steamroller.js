b83b52281e02ee703bfbacc656fbaa89b4b9433a
Cailyn <cailyn.s.e@gmail.com>
2017-03-23 11:51:31 -0400

    Actually finished

--- steamroller.js
+++ steamroller.js
@@ -1,6 +1,14 @@
 function steamrollArray(arr) {
     // I'm a steamroller, baby
-    return arr;
+    var mergedArr = [].concat.apply([], arr);
+
+    for (var i = 0; i < mergedArr.length; i++) {
+        while (Array.isArray(mergedArr[i])) {
+            mergedArr = [].concat.apply([], mergedArr);
+        }
+    }
+
+    return mergedArr;
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
